c4674a7837ed2b4095d69a40866225e936beb06d
AlexandreFVieira <falecomalex204@gmail.com>
2024-01-20 15:23:39 -0300

    Melhoria no sistema de ataque e no código

--- Final-Project-DIO.js
+++ Final-Project-DIO.js
@@ -1,23 +1,43 @@
-// INICIO DE PROGRAMAÇÃO
-
-//Nao esquecer de colocar o console.log
 class ClasseDoHeroi {
-    constructor (type, attack, name, age){
-        this.type = type
-        this.attack = attack
-        this.name = "Agron"
-        this.age = 2200
+    constructor(type, name, age) {
+        this.type = type;
+        this.name = name;
+        this.age = age;
+        this.attack = this.definirAtaque();
     }
-    atacar(){
-            console.log(`O ${this.type} atacou usando ${this.attack}. `)
+
+    definirAtaque() {
+        switch (this.type.toLowerCase()) {
+            case 'mago':
+                return 'magia';
+            case 'guerreiro':
+                return 'espada';
+            case 'monge':
+                return 'artes marciais';
+            case 'ninja':
+                return 'shuriken';
+            default:
+                return 'ataque desconhecido';
+        }
+    }
+
+    atacar() {
+        console.log(`O ${this.type} ${this.name} atacou usando ${this.attack}.`);
     }
 }
 
-let Mago = new ClasseDoHeroi("Mago","magia" )
-let Guerreiro = new ClasseDoHeroi("Guerreiro", "espada")
-let Monge = new ClasseDoHeroi("Monge", "artes marciais")
-let Ninja = new ClasseDoHeroi("Ninja", "shuriken")
-let Arqueiro = new ClasseDoHeroi("Arqueiro", "arco e flecha")
+let herois = {
+    mago: new ClasseDoHeroi("Mago", "Gandalf", 330),
+    guerreiro: new ClasseDoHeroi("Guerreiro", "Gutts", 24),
+    monge: new ClasseDoHeroi("Monge", "Dalai-Lama", 98),
+    ninja: new ClasseDoHeroi("Ninja", "Naruto", 20),
+    arqueiro: new ClasseDoHeroi("Arqueiro", "Legolas", 300)
+};
 
-Arqueiro.atacar()
+let classeEscolhida = "mago"; // Classe que deseja exibir
 
+if (herois[classeEscolhida]) {
+    herois[classeEscolhida].atacar();
+} else {
+    console.log("Classe não encontrada.");
+}
